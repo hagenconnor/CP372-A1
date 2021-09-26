b947d1d52f13190c72b44ef95038bdda5d45e44c
hagenconnor <hagenconnor6@gmail.com>
2021-09-26 17:25:36 -0400

    Added note class and definitions

---
+++ Note.java
@@ -0,0 +1,22 @@
+public class Note {
+    private String bottom_coord;
+    private int width;
+    private int height;
+    private String colour;
+    private String message;
+    private int pin_status;
+
+    public Note (String bottom_coord, int width, int height, String colour, String message, int pin_status){
+        this.bottom_coord = bottom_coord;
+        this.width = width;
+        this.height = height;
+        this.colour = colour;
+        this.message = message;
+        this.pin_status = pin_status;
+    }
+
+    public String get_note_details(){
+        
+        return null;
+    }
+}
