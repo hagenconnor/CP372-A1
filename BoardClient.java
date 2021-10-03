31deaea601c6206af8143b860ecf09b9b9486d51
hagenconnor <hagenconnor6@gmail.com>
2021-10-03 16:35:04 -0400

    More work on server-client communication

--- BoardClient.java
+++ BoardClient.java
@@ -14,12 +14,11 @@ public class BoardClient {
     JFrame frame = new JFrame("BoardClient");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(800,200);
-
+    
+    //Connection panel.
     JPanel connection = new JPanel();
-
     JButton connect_button = new JButton("Connect");
     JButton disconnect_button = new JButton("Disconnect");
-
     JTextField server_ip = new JTextField();
     server_ip.setPreferredSize( new Dimension( 200, 24 ) );
     JLabel ip_label = new JLabel("IP Address: ");
@@ -27,15 +26,27 @@ public class BoardClient {
     server_port.setPreferredSize( new Dimension( 200, 24 ) );
     JLabel port_label = new JLabel("Port #: ");
 
+    //Results panel.
     JPanel interaction = new JPanel();
-
     JTextArea results = new JTextArea();
+    results.setPreferredSize( new Dimension( 200, 200 ) );
+    
+    JPanel board_panel = new JPanel();
+    JTextField commands = new JTextField();
+    commands.setPreferredSize( new Dimension( 200, 24 ) );
+    JButton post = new JButton("POST");
+    JButton get = new JButton("GET");
+    JButton pin = new JButton("PIN");
+    JButton unpin = new JButton("UNPIN");
+    JButton clear = new JButton("CLEAR");
+    JButton shake = new JButton("SHAKE");
 
     connect_button.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed (ActionEvent e){
             try {
-                openConnection(server_ip.getText(), Integer.parseInt(server_port.getText()));
+                String status = openConnection(server_ip.getText(), Integer.parseInt(server_port.getText()));
+                results.setText(status);
             } catch (NumberFormatException e1) {
                 // TODO Auto-generated catch block
                 e1.printStackTrace();
@@ -60,7 +71,13 @@ public class BoardClient {
             }
         }
     });
-
+    get.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed (ActionEvent e){
+            out.println("Get pressed - Test message to server");
+        }
+    });
+    //Connection panel.
     connection.add(connect_button);
     connection.add(disconnect_button);
     connection.add(ip_label);
@@ -68,16 +85,28 @@ public class BoardClient {
     connection.add(port_label);
     connection.add(server_port);
 
+    //Results panel
     interaction.add(results);
 
+    //Command panel
+    board_panel.add(commands);
+    board_panel.add(post);
+    board_panel.add(get);
+    board_panel.add(pin);
+    board_panel.add(unpin);
+    board_panel.add(clear);
+    board_panel.add(shake);
+    
+    //Add panels to frame.
     frame.getContentPane().add(BorderLayout.NORTH,connection);
     frame.getContentPane().add(BorderLayout.CENTER,interaction);
+    frame.getContentPane().add(BorderLayout.SOUTH, board_panel);
 
     frame.setVisible(true);
     
     }
 
-    static void openConnection(String ip, int port) throws IOException{
+    static String openConnection(String ip, int port) throws IOException{
         try {
             boardSocket = new Socket(ip, port);
             out = new PrintWriter(boardSocket.getOutputStream(), true);
@@ -92,11 +121,15 @@ public class BoardClient {
 
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         String fromServer;
-
+        
         if ((fromServer = in.readLine()) != null) {
-            System.out.println("Server: " + fromServer);
+            String text = fromServer;
+            stdIn.close();
+            return text;
         }
-        stdIn.close();
+        else{
+            return "error connecting";
+        }
 
     }
     static void closeConnection() throws IOException{
--- BoardSession.java
+++ BoardSession.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class BoardSession implements Runnable{
     Socket socket;
+    InputStream is;
+    OutputStream os;
+    BufferedReader stdIn;
     public static void main(String args[]){
         
     }
@@ -11,6 +14,14 @@ public class BoardSession implements Runnable{
         try {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println("Connected successfully");
+            stdIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            try {
+                startBoard();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -19,5 +30,18 @@ public class BoardSession implements Runnable{
     }
     public BoardSession (Socket socket) throws Exception{
         this.socket = socket;
+    }
+
+    public void startBoard() throws Exception{
+        String fromClient;
+        while (true){
+            if ((fromClient = stdIn.readLine()) != null){
+                //ADD BOARD CODE HERE -- this is our BOARD mechanics
+                System.out.println("Get pressed -- Test message to server");
+                
+            }
+        }
+        
+
     }
 }
